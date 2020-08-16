3cfa7fd8d9bc4d789ddfdee6f93a0d5ce26d955d
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-16 11:37:40 +0300

    added the code to calculate 2 numebers

--- calculator.py
+++ calculator.py
@@ -1,7 +1,39 @@
 
 def main():
 	#write your code here
-	pass
+	num_1 = 0
+	num_2 = 0
+	
+
+	num_1 = input("Enter the first number: ")
+
+	while not num_1.isnumeric():
+		print("enter a valid number!")
+		num_1 = input("Enter the first number: ")
+
+	num_2 = input("Enter the second number: ")
+
+	while not num_2.isnumeric():
+		print("enter a valid number!")
+		num_2 = input("Enter the second number: ")
+
+	operator = input("Choose the operation (+, -, /, *): ")
+
+	if operator == "+":
+    		answer = int(num_1) + int(num_2)
+    		print ("The answer is " + str(int(answer)))
+	elif operator == "-":
+    		answer = int(num_1) - int(num_2)
+    		print ("The answer is " + str(int(answer)))
+	elif operator == "/":
+    		answer = float(num_1) / float(num_2)
+    		print ("The answer is " + str(int(answer)))
+	elif operator == "*":
+    		answer = int(num_1) * int(num_2)
+    		print ("The answer is " + str(int(answer)))
+	else:
+    		print("please choose an operator")
+    		
 	
 
 
