c4733191c74884921f0c275dafc4f94171077916
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-17 09:30:17 +0300

    working version with if and nested if

--- calculator.py
+++ calculator.py
@@ -1,39 +1,35 @@
 
 def main():
 	#write your code here
+	operators = ["+","-","/","*"]
+
 	num_1 = input("Enter the first number: ")
-
-	while not num_1.isnumeric():
-		print("enter a valid number!")
-		num_1 = input("Enter the first number: ")
-
 	num_2 = input("Enter the second number: ")
-
-	while not num_2.isnumeric():
-		print("enter a valid number!")
-		num_2 = input("Enter the second number: ")
-
 	operator = input("Choose the operation (+, -, /, *): ")
-	opertators = ["+","-","/","*"]
-	while operator not in opertators:
-		print("operator is not valid")
-		operator = input("Choose the operation (+, -, /, *): ")
 
-	if operator == "+":
-    		answer = int(num_1) + int(num_2)
-    		print ("The answer is " + str(int(answer)))
-	elif operator == "-":
-    		answer = int(num_1) - int(num_2)
-    		print ("The answer is " + str(int(answer)))
-	elif operator == "/":
-    		answer = float(num_1) / float(num_2)
-    		print ("The answer is " + str(int(answer)))
-	elif operator == "*":
-    		answer = int(num_1) * int(num_2)
-    		print ("The answer is " + str(int(answer)))
-	
+
+	if not num_1.isdigit() or not num_2.isdigit() or operator not in operators:
+		print("please enter a valid number")
+	else:
+		# cast to int
+		num_1 = int(num_1) 
+		num_2 = int(num_2) 
+		result = 0
+		# do the operation
+		if operator == "+":
+			result = num_1+num_2
+		elif operator == "-":
+			result = num_1-num_2
+		elif operator == "/":
+			result = int(float(num_1)/float(num_2))
+		elif operator == "*":
+			result = num_1*num_2
+		
+		print("the answer is " + str(result))
     		
-	
+
+
+
 
 if __name__ == '__main__':
 	main()
